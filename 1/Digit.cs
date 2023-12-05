55e29def742f6465dd64a5ad41312a9bc5625d5a
Johan Bjärneryd <johan.bjarneryd@sogeti.com>
2023-12-05 17:36:49 +0100

    Case 1

---
+++ 1/Digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Digit
+    {
+        public Digit(int column, char value)
+        {
+            Column = column;
+            Value = value;
+        }
+
+        public int Column { get; set; }
+        public char Value { get; set; }
+    }
+}
--- 1/Program.cs
+++ 1/Program.cs
@@ -8,133 +8,99 @@ namespace _1
 {
     internal class Program
     {
-        private static IDictionary<string, int> lookups;
+        private static IDictionary<string, char> lookups;
         static void Main(string[] args)
         {
+            IList<IList<Digit>> processedRows = new List<IList<Digit>>();
+            IList<string> lines = new List<string>();
             var line = Console.ReadLine();
             lookups = SetupLookup();
             var numbers = new List<int>();
 
             while (!string.IsNullOrEmpty(line))
             {
-                string firstDigit = null, lastDigit = null;
-                int? fd = null, ld = null;
-                int? firstIndex = null, lastIndex = null;
-                
-                for(int i=0;i<line.Length; i++)
-                {
-                    var sub = line.Substring(0, i+1);
-                    var res = FindDigit(sub);
-                    if (!string.IsNullOrEmpty(res))
-                    {
-                        firstIndex = line.IndexOf(res);
-                        fd = lookups[res];
-                        break;
-                    }
-                }
-
-                for (int i = line.Length; i >= 0; i--)
-                {
-                    var sub = line.Substring(i);
-                    var res = FindDigit(sub);
-                    if (!string.IsNullOrEmpty(res))
-                    {
-                        lastIndex = line.LastIndexOf(res);
-                        ld = lookups[res];
-                        break;
-                    }
-                }
-        
-                for (int i = 0; i < line.Length; i++)
-                {
-                    if(fd == null && IsNumber(line[i].ToString()))
-                    {
-                        firstDigit = line[i].ToString();
-                        break;
-                    }
-                    else if(fd != null && IsNumber(line[i].ToString()))
-                    {
-                        if (firstIndex > i)
-                        {
-                            firstDigit = line[i].ToString();
-                        }
-                        else
-                        {
-                            firstDigit = fd.ToString();
-                        }
-                        break;
-                    }
-                }
-
-                for (int i = line.Length; i > 0; i--)
-                {
-                    if (ld == null && IsNumber(line[i-1].ToString()))
-                    {
-                        lastDigit = line[i-1].ToString();
-                        break;
-                    }
-                    else if (ld != null && IsNumber(line[i-1].ToString()))
-                    {
-                        if (lastIndex < i-1)
-                        {
-                            lastDigit = line[i-1].ToString();
-                        }
-                        else
-                        {
-                            lastDigit = ld.ToString();
-                        }
-                        break;
-                    }
-                }
-
-                numbers.Add(Convert.ToInt32("" + firstDigit + lastDigit));
+                lines.Add(line);
                 line = Console.ReadLine();
             }
 
-            var sum = numbers.Sum();
-             Console.WriteLine(sum);
-        }
+            foreach (var lineToProcess in lines)
+            {
+                var digits = new List<Digit>();
+                for (int i = 0; i < lineToProcess.Length; i++)
+                {
+                    if (IsDigit(lineToProcess[i]))
+                    {
+                        digits.Add(new Digit(i, lineToProcess[i]));
+                    }
+                }
+                var fromStart = FindDigit(lineToProcess);
+                if (!string.IsNullOrEmpty(fromStart))
+                {
+                    var index = lineToProcess.IndexOf(fromStart);
+                    digits.Add(new Digit(index, lookups[fromStart]));
+                }
 
-        private static bool IsNumber(string c)
-        {
-            int num;
-            return int.TryParse(c, out num);
+                for (int i = lineToProcess.Length; i >= 0; i--)
+                {
+                    var sub = lineToProcess.Substring(i);
+                    var fromBack = FindDigit(sub);
+                    if (!string.IsNullOrEmpty(fromBack))
+                    {
+                        var index = lineToProcess.LastIndexOf(fromBack);
+                        digits.Add(new Digit(index, lookups[fromBack]));
+                    }
+                }
+
+                digits = digits.OrderBy(x => x.Column).ToList();
+                var xNum = "" + digits.First().Value + digits.Last().Value;
+                numbers.Add(Convert.ToInt32(xNum));
+
+            }
+
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
 
         private static string FindDigit(string s)
         {
-            var match = Regex.Match(s.ToLower(), @"zero|one|two|three|four|five|six|seven|eight|nine");
-            if(match.Success)
+            var match = Regex.Match(s.ToLower(), @"one|two|three|four|five|six|seven|eight|nine");
+            if (match.Success)
             {
                 return match.Value.ToString();
             }
             return null;
         }
 
-        private static IDictionary<string,int> SetupLookup()
+        private static IDictionary<string, char> SetupLookup()
         {
-            var dict = new Dictionary<string, int>();
-            dict.Add("zero", 0);
-            dict.Add("0", 0);
-            dict.Add("one", 1);
-            dict.Add("1", 1);
-            dict.Add("two", 2);
-            dict.Add("2", 2);
-            dict.Add("three", 3);
-            dict.Add("3", 3);
-            dict.Add("four", 4);
-            dict.Add("4", 4);
-            dict.Add("five", 5);
-            dict.Add("5", 5);
-            dict.Add("six", 6);
-            dict.Add("6", 6);
-            dict.Add("seven", 7);
-            dict.Add("7", 7);
-            dict.Add("eight", 8);
-            dict.Add("8", 8);
-            dict.Add("nine", 9);
-            dict.Add("9", 9);
+            var dict = new Dictionary<string, char>();
+            dict.Add("one", '1');
+            dict.Add("two", '2');
+            dict.Add("three", '3');
+            dict.Add("four", '4');
+            dict.Add("five", '5');
+            dict.Add("six", '6');
+            dict.Add("seven", '7');
+            dict.Add("eight", '8');
+            dict.Add("nine", '9');
             return dict;
+        }
+
+        private static bool IsDigit(char c)
+        {
+            return char.IsDigit(c);
+        }
+
+        private static void FirstStar(IList<string> lines)
+        {
+            var firstStarNumbers = new List<int>();
+            //first star
+            foreach (var lineToProcess in lines)
+            {
+                var number = $"{lineToProcess.First(x => char.IsDigit(x))}{lineToProcess.Last(x => char.IsDigit(x))}";
+                firstStarNumbers.Add(Convert.ToInt32(number));
+            }
+            Console.WriteLine("First star result = " + firstStarNumbers.Sum());
         }
     }
 }
