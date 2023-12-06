f8882adaad9339e8701095454993314eba4ca91f
Johan <johan@bjarneryd.se>
2023-12-06 21:21:59 +0100

    Add project for 7

---
+++ 7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> input = ReadInput();
+        }
+        private static IList<string> ReadInput()
+        {
+            var inData = new List<string>();
+            using (StreamReader sr = new StreamReader(@"testdata0.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    inData.Add(sr.ReadLine());
+                }
+            }
+            return inData;
+        }
+
+    }
+}
